37619244b8f5f61345ab06e460aaa807d256f684
Erlend Hamberg <ehamberg@comoyo.com>
2013-07-10 19:53:11 +0200

    remove cmdargs as its overkill for this simple program

--- watcher.hs
+++ watcher.hs
@@ -1,19 +1,17 @@
-{-# Language OverloadedStrings, DeriveDataTypeable #-}
-
-import System.Posix.Files
+import System.IO (hPutStrLn, stderr)
+import System.Posix.Files (getFileStatus, isDirectory)
+import System.Environment (getArgs, getProgName)
 import System.Directory (canonicalizePath)
 import Filesystem.Path (directory)
 import Data.String (fromString)
-import System.FSNotify
-import System.Exit (exitSuccess)
-import System.Process
-import Control.Monad (void)
+import System.FSNotify (Event (..), WatchManager, startManager, stopManager, watchTree, watchDir)
+import System.Exit (exitSuccess, exitFailure)
+import System.Process (createProcess, proc)
+import Control.Monad (void, when)
 import System.Posix.Signals (installHandler, Handler(Catch), sigINT, sigTERM)
 import Control.Concurrent.MVar
-import System.Console.CmdArgs
 
 data Watcher = Watcher { fspath :: FilePath , command :: String }
-  deriving (Data,Typeable,Show)
 
 data FileType = File | Directory deriving Eq
 
@@ -22,23 +20,19 @@ runCmd cmd _ = do
   putStrLn $ "Running " ++ cmd
   void (createProcess (proc cmd [])) -- TODO: get notified when process ends and print "Waiting..."
 
-watcher :: Mode (CmdArgs Watcher)
-watcher = cmdArgsMode $ Watcher
-  { fspath  = ""   &= argPos 0 &= typ "<file/directory to watch>"
-  , command = ""   &= argPos 1 &= typ "<command to run>"}
-  &= summary "watcher: run a command when a file/directory is modified"
-
-
 watch :: FileType -> WatchManager -> String -> String -> IO ()
 watch Directory m path cmd  = watchTree m (fromString path) (const True) (runCmd cmd)
 watch File m path cmd       = watchDir m (directory $ fromString path) isThisFile (runCmd cmd)
   where isThisFile (Modified p _) = p == fromString path
         isThisFile _              = False
 
-
 main :: IO ()
 main = do
-  argv <- cmdArgsRun watcher
+  args <- getArgs
+  when (length args /= 2) $ getProgName >>= usage >> exitFailure
+
+  let [path, cmd] = args
+  let argv = Watcher path cmd
 
   m <- startManager
 
@@ -62,3 +56,6 @@ main = do
   putStrLn "\nStopping."
   stopManager m
   exitSuccess
+    where usage n = hPutStrLn stderr $ "Usage: " ++ n
+                             ++ " <file/directory to watch>"
+                             ++ " <command to run>"
